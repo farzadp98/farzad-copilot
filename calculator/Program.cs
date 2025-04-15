1a8b0db92a74c4080b11ad053f07807d098a6425
farzadp98 <53206616+farzadp98@users.noreply.github.com>
2025-04-15 16:24:38 +0330

    first commit

---
+++ calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using calculator;
+
+Console.WriteLine("Hello, World!");
+
+
+numbers num = new numbers
+{
+    num1 = 4,
+    num2 = 5,
+    op = "/"
+};
+
+double resault = 0;
+
+
+if(num.op== "+")
+{
+    resault = num.num1 + num.num2;
+}
+else if (num.op == "-")
+{
+    resault = num.num1 - num.num2;
+}
+else if (num.op == "*")
+{
+    resault = num.num1 * num.num2;
+}
+else if (num.op == "/")
+{
+    resault = num.num1 / num.num2;
+}
+else
+{
+    Console.WriteLine("Invalid operator");
+};
+
+Console.WriteLine("The result is: " + resault);
---
+++ calculator/numbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class numbers
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+
+        public string op { get; set; }
+    }
+
+   
+}
